2397604805e394a61fac70aa63847657c239488f
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-06-08 14:21:09 +0300

    subtask 2

--- soldier.js
+++ soldier.js
@@ -1,19 +1,34 @@
 const soldier = {
   name: 'Adam',
-  health: 10,
+  health: 0,
   gun: {
     nameGun: 'Стечкин',
-    numberBullets: 3
+    numberBullets: 0,
   },
-  provisions: 4,
-  
+  provisions: 3,
+
   fire: function() {
-  
+    if (this.gun.numberBullets <= 0) {
+      console.log('обойма пуста. Перезаредитесь');
+    } else {
+      console.log('бах-бах');
+      this.gun.numberBullets--;
+    }
   },
   recharge: function() {
- 
+    if (this.provisions <= 0) {
+      console.log(`Не осталось припасов`);
+    } else {
+      this.gun.numberBullets = 30;
+      this.provisions--;
+      console.log(`Перезарядка...`);
+    }
   },
   hurt: function() {
-    
-  }
+    if (this.health <= 0) {
+      console.log('Ты проиграл');
+    } else {
+      this.health--;
+    }
+  },
 }
