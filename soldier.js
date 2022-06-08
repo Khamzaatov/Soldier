1bc908355da1e1684623adf3ba854ec4100d2f9c
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-06-08 14:19:54 +0300

    subtask 1

---
+++ soldier.js
@@ -0,0 +1,19 @@
+const soldier = {
+  name: 'Adam',
+  health: 10,
+  gun: {
+    nameGun: 'Стечкин',
+    numberBullets: 3
+  },
+  provisions: 4,
+  
+  fire: function() {
+  
+  },
+  recharge: function() {
+ 
+  },
+  hurt: function() {
+    
+  }
+}
